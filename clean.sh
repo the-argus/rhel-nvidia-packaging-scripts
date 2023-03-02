dfc07c0bfa40ade0e6432f8a80b184e55773be6d
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-03-02 17:13:54 -0500

    source files relative to script location

--- clean.sh
+++ clean.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-source ./config
+SCRIPT_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$SCRIPT_DIR" ]]; then SCRIPT_DIR="$PWD"; fi
+source $SCRIPT_DIR/config
 
 if [ "$1" == "deep" ]; then
     rm -rf $BUILDDIR
